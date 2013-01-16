a55e6cd14c5dfc3cf3f8893debc384e81b742d68
Ben <benjaminacarlson@hotmail.com>
2013-01-16 02:14:55 -0800

    Implemented some of the core parts of the game so that it will be easy to see progress in the future

---
+++ requestHandler.js
@@ -0,0 +1,18 @@
+
+var api = require('./availableRequests');
+/**
+ * @params object request {
+ *	string r request to the server
+ *	mixed p params to the request
+ * }
+ * @returns mixed response the response of the client
+ */
+exports.handle = function(request, lib) {
+	request.response = "";
+	if(api[request.r.lib]) {
+		console.log(request);
+		request.response = api[request.r.lib][request.r.func](request.p, lib);
+		return request;
+	}
+	return request;
+}
